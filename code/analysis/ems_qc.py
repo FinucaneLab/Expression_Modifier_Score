4e711dcf5fd0ecdebb21534d71b5404f7f8bcd04
QingboWang <QingboWang@users.noreply.github.com>
2020-09-07 23:15:24 -0400

    Update ems_qc.py

--- code/analysis/ems_qc.py
+++ code/analysis/ems_qc.py
@@ -56,3 +56,83 @@ plt.tight_layout()
 fn = "gs://qingbowang/ems_v1_test/agg_for_fig1/bas_featimp.png"
 with hl.hadoop_open(fn, 'wb') as f:
     plt.savefig(f, dpi=500)
+
+    
+#plot EMS per tissue:
+tissues = ["Whole_Blood",
+            "Muscle_Skeletal",
+           "Liver",
+           "Brain_Cerebellum",
+            "Prostate",
+            "Spleen",
+            "Skin_Sun_Exposed_Lower_leg",
+            "Artery_Coronary",
+            "Esophagus_Muscularis",
+            "Esophagus_Gastroesophageal_Junction",
+            "Artery_Tibial",
+            "Heart_Atrial_Appendage",
+            "Nerve_Tibial",
+            "Heart_Left_Ventricle",
+            "Adrenal_Gland",
+            "Adipose_Visceral_Omentum",
+            "Pancreas",
+            "Lung",
+            "Pituitary",
+            "Brain_Nucleus_accumbens_basal_ganglia",
+            "Colon_Transverse",
+            "Adipose_Subcutaneous",
+            "Esophagus_Mucosa",
+            "Brain_Cortex",
+            "Thyroid",
+            "Stomach",
+            "Breast_Mammary_Tissue",
+            "Colon_Sigmoid",
+            "Skin_Not_Sun_Exposed_Suprapubic",
+            "Testis",
+            "Artery_Aorta",
+            "Brain_Amygdala",
+            "Brain_Anterior_cingulate_cortex_BA24",
+            "Brain_Caudate_basal_ganglia",
+            "Brain_Cerebellar_Hemisphere",
+            "Brain_Frontal_Cortex_BA9",
+            "Brain_Hippocampus",
+            "Brain_Hypothalamus",
+            "Brain_Putamen_basal_ganglia",
+            "Brain_Spinal_cord_cervical_c-1",
+            "Brain_Substantia_nigra",
+            "Cells_Cultured_fibroblasts",
+            "Cells_EBV-transformed_lymphocytes",
+            "Kidney_Cortex",
+            "Minor_Salivary_Gland",
+            "Ovary",
+            "Small_Intestine_Terminal_Ileum",
+            "Uterus",
+            "Vagina"]#all 49 tissues
+rocs = []
+prcs = []
+for tissue_name in tissues:
+    fn = "gs://qingbowang/ems_v1_test/grid_search_result_{0}.tsv".format(tissue_name)
+    with hl.hadoop_open(fn, 'r') as f: 
+        df = pd.read_csv(f, sep="\t")
+    rocs.append(df.auroc.values[0])
+    prcs.append(df.auprc.values[0])
+roc = pd.DataFrame({"roc":rocs,"prc":prcs})
+roc.index = tissues
+#add the sample size...
+npos = []
+for tissue_name in tissues:
+    fn = "gs://qingbowang/ems_v1_test/{0}_positive_training_vg_beforeannot.tsv".format(tissue_name)
+    with hl.hadoop_open(fn, 'r') as f:
+            df = pd.read_csv(f, sep="\t")
+    npos.append(df.shape[0])
+roc["n_pos"] = npos
+#plot:
+from scipy import stats
+x = roc.n_pos
+y = roc.roc
+plt.scatter(x,y)
+(r,p) = stats.pearsonr(x, y)
+plt.xlabel("n(positive sample)")
+plt.ylabel("AUROC")
+plt.title("(pearson r, p-value) = ({0},{1})".format(str(r)[:6], str(p)[:6]))
+plt.show()
